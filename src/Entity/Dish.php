c8cfdf213d834cbad31dbdabacf417af1f29caa2
Michael <coucou@localhost.com>
2021-03-29 10:23:37 -0400

    Adding constraint for Dish

--- src/Entity/Dish.php
+++ src/Entity/Dish.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\DishRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=DishRepository::class)
@@ -19,16 +20,22 @@ class Dish
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Type("string")
      */
     private $name;
 
     /**
      * @ORM\Column(type="decimal", precision=10, scale=2)
+     * @Assert\NotBlank()
+     * @Assert\Type("numeric")
+     * @Assert\Positive()
      */
     private $price = 0;
 
     /**
      * @ORM\ManyToOne(targetEntity=Restaurant::class, inversedBy="dishes")
+     * @Assert\Valid();
      */
     private $restaurant;
 
